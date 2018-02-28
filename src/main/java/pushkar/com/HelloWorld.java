4f33d50d1041ec72b89cb7750b9e12c3ed0b8969
pushakrroushan <pushakrroushancs@gmail.com>
2018-03-01 03:25:42 +0530

    commited

---
+++ src/main/java/pushkar/com/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package pushkar.com;
+
+/**
+ * 
+ * 
+ */
+public class HelloWorld {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+    
+}
